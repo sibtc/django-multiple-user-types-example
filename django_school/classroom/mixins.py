c70391b6319111fb12dfaf939fd5aff1e4f98e12
ceoro9 <ceoro9@protonmail.com>
2018-05-15 22:14:19 +0300

    changed user check functionality

--- django_school/classroom/mixins.py
+++ django_school/classroom/mixins.py
@@ -1,20 +1,16 @@
 """
     Mixins for class-based views,
-    that check user role, they count
-    on decorators logic functionality!
+    that check user auth. and role
 """
 
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth.decorators import login_required
-
-from .decorators import student_required, teacher_required
 
 
 class StudentRequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return login_required() and student_required()
+        return self.request.user.is_authenticated and self.request.user.is_student
 
 
 class TeacherRequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return login_required() and teacher_required()
+        return self.request.user.is_authenticated and self.request.user.is_teacher
