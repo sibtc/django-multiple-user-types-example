b3aa568b2e46957806791a6b3526a794946f1ff2
suhailvs <suhailvs@gmail.com>
2019-05-14 22:12:48 +0530

    this closes #5, logged in user can access login url

--- django_school/django_school/urls.py
+++ django_school/django_school/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 
 from classroom.views import classroom, students, teachers
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('classroom.urls')),
+    path('accounts/login/', auth_views.login, name='login', kwargs={'redirect_field_name': '/', 'redirect_authenticated_user': True}),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
