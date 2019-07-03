3811fa3a202e7b6768b3836dd80da0f7098a22c4
suhailvs <suhailvs@gmail.com>
2019-07-03 15:57:13 +0530

    updated requirements and added login redirect for authusers

--- django_school/django_school/urls.py
+++ django_school/django_school/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('classroom.urls')),
-    path('accounts/login/', auth_views.login, name='login', kwargs={'redirect_field_name': '/', 'redirect_authenticated_user': True}),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
