0de01aec7f4dcfb5d940f3c1c8992075a2e00e0c
suhailvs <suhailvs@gmail.com>
2020-04-24 08:58:22 +0530

    fixed redirect to login

--- django_school/classroom/views/classroom.py
+++ django_school/classroom/views/classroom.py
@@ -10,6 +10,8 @@ def home(request):
     if request.user.is_authenticated:
         if request.user.is_teacher:
             return redirect('teachers:quiz_change_list')
-        else:
+        elif request.user.is_student:
             return redirect('students:quiz_list')
+        else:
+            return redirect('admin:index')
     return render(request, 'classroom/home.html')
--- django_school/django_school/urls.py
+++ django_school/django_school/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 	path('admin/', admin.site.urls),
     path('', include('classroom.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    # path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
