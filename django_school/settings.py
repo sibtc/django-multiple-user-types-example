fda777db7bc99a08a5abfe46dc615e8c90b0e474
Hafsa Javed <hafsajavedpak@outlook.com>
2024-02-04 09:08:56 +0500

    configruing STATIC_ROOT

--- django_school/settings.py
+++ django_school/settings.py
@@ -110,13 +110,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    ('', os.path.join(BASE_DIR, 'static')),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # CHAGE MADE ACCORDING TO CHATPGT
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'collected_static')
